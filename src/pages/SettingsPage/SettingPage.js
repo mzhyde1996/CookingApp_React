db98af2d908c6ebd3f8e99912424b72ee2ca0911
Vera <101629174+mzhyde1996@users.noreply.github.com>
2024-02-15 14:33:56 +0200

    Update SettingPage.js

--- src/pages/SettingsPage/SettingPage.js
+++ src/pages/SettingsPage/SettingPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './SettingPage.css'
 import Header from '../../components/Header/Header'
